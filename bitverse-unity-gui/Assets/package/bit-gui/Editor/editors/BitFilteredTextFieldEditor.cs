ec55b87eafccf411b0cf7b71f0976421f8e3ea6c
daniel.sperry <daniel.sperry@localhost>
2010-07-28 02:53:45 -0300

    Fixed component editosr problem with unity3d 3.0

---
+++ bitverse-unity-gui/Assets/package/bit-gui/Editor/editors/BitFilteredTextFieldEditor.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using Bitverse.Unity.Gui;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+[CustomEditor(typeof(BitFilteredTextField))]
+public class BitFilteredTextFieldEditor : BitControlEditor
+{
+} 
+ 
